8f91f4823e670ebc060164be06cad91ad4b5f0bf
Daniel Lewis <daniel.lewis@protonmail.com>
2021-03-25 13:11:00 -0600

    added player object properties

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,6 @@ const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-
 console.log(new Potion());
 
 test('creates a player object', () => {
@@ -34,4 +33,26 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+    expect(player.isAlive()).toBeTruthy();
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth - 5);
+    player.reduceHealth(999999);
+    expect(player.health).toBe(0);
 });
